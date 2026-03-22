f198c17ee0758578cd1a0297832389007a9fc290
poswalsameer <poswalsameer2003@gmail.com>
2026-03-22 14:40:28 +0530

    testing whole flow one more time

--- examples/test-app/index.tsx
+++ examples/test-app/index.tsx
@@ -1,10 +1,23 @@
+import React from "react"
 import { render } from "ink"
-import Text from "./src/components/shellcn/text"
-
+import { Alert } from "./src/components/shellcn/alert.js"
 
 const App = () => {
   return (
-    <Text color="green" italic>Hello world</Text>
+    <>
+      <Alert variant="info" title="Welcome to shellcn!">
+        You have successfully initialized the library and added the alert component.
+      </Alert>
+      <Alert variant="success" radius="round" alignItems="center">
+        Everything is working perfectly.
+      </Alert>
+      <Alert variant="warning" borderStyle="double">
+        This is a warning alert with a double border.
+      </Alert>
+      <Alert variant="error" radius="none" borderColor="magenta">
+        Custom error alert with a magenta border!
+      </Alert>
+    </>
   )
 }
 
